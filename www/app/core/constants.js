735d73125a529a4799a7410bf151a1e139d477b1
Junior Oliveira <junolive@gmail.com>
2015-12-30 17:51:41 -0200

    plugin cordova-plugin-vibration

--- www/app/core/constants.js
+++ www/app/core/constants.js
@@ -7,6 +7,7 @@
     .constant('cordova', typeof window.cordova !== 'undefined' ? window.cordova : {})
     .constant('shake', typeof window.shake !== 'undefined' ? window.shake : {})
     .constant('insomnia', (typeof window.plugins !== 'undefined') && (typeof window.plugins.insomnia !== 'undefined') ? window.plugins.insomnia : {})
+    .constant('navigator', typeof navigator !== 'undefined' ? navigator : {})
     .constant('cards', ['0', '1', '2', '3', '5', '8', '13', '20', '40', '&infin;', '?']);
 
 })();
