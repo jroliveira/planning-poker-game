4c4a213bfa290c60a4c60c9f7df0767539602ddb
Junior Oliveira <junolive@gmail.com>
2019-10-24 23:11:16 +0100

    Updates

---
+++ precache-manifest.3d373f9e949ce7fa4bbd459cb53dbcec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "84990fc2db3c8f55df26",
+    "url": "/planning-poker-game/static/css/main.393801a5.chunk.css"
+  },
+  {
+    "revision": "84990fc2db3c8f55df26",
+    "url": "/planning-poker-game/static/js/main.84990fc2.chunk.js"
+  },
+  {
+    "revision": "94ad056ff3df336903fc",
+    "url": "/planning-poker-game/static/js/runtime~main.94ad056f.js"
+  },
+  {
+    "revision": "a8bc664fbb9b60c6f07f",
+    "url": "/planning-poker-game/static/css/2.f3b451d4.chunk.css"
+  },
+  {
+    "revision": "a8bc664fbb9b60c6f07f",
+    "url": "/planning-poker-game/static/js/2.a8bc664f.chunk.js"
+  },
+  {
+    "revision": "5442b008cf7eb4d0babe804266535886",
+    "url": "/planning-poker-game/index.html"
+  }
+];
