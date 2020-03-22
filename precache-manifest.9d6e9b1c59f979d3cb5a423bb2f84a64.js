87902437713639d520693941e4f218acb4e4bc6e
Junior Oliveira <junolive@gmail.com>
2020-03-22 16:57:28 +0000

    Updates

---
+++ precache-manifest.9d6e9b1c59f979d3cb5a423bb2f84a64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f2ddc8fb75e32c2825186d1aad91613",
+    "url": "/planning-poker-game/index.html"
+  },
+  {
+    "revision": "8b4821e2011249a3c36f",
+    "url": "/planning-poker-game/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "37c42b891013158fc763",
+    "url": "/planning-poker-game/static/css/main.812a1d76.chunk.css"
+  },
+  {
+    "revision": "8b4821e2011249a3c36f",
+    "url": "/planning-poker-game/static/js/2.19f1de0e.chunk.js"
+  },
+  {
+    "revision": "df958f16b07871c3679510165e97a61a",
+    "url": "/planning-poker-game/static/js/2.19f1de0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37c42b891013158fc763",
+    "url": "/planning-poker-game/static/js/main.e0778485.chunk.js"
+  },
+  {
+    "revision": "546f032982ebefec5a7c",
+    "url": "/planning-poker-game/static/js/runtime-main.710428e6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/planning-poker-game/precache-manifest.3d373f9e949ce7fa4bbd459cb53dbcec.js"
+  "/planning-poker-game/precache-manifest.9d6e9b1c59f979d3cb5a423bb2f84a64.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/planning-poker-game/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/planning-poker-game/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
