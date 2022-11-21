7fa550c53b2b9e43f5f5c1cfb2f6794effd913dc
Junior Oliveira <junolive@gmail.com>
2022-11-21 11:21:43 +0000

    fix: some bugs

--- eslint-config/rules/css-modules.js
+++ eslint-config/rules/css-modules.js
@@ -2,12 +2,7 @@
 
 module.exports = {
   rules: {
-    // Force use of all the classes defined in your css/scss/less file
-    // It's a warning because it gives some false positives
-    // See https://gitlab.fftech.info/mondrian/eslint-config/issues/26
     'css-modules/no-unused-class': 1,
-
-    // Disallow use of non-existing classes
     'css-modules/no-undef-class': 1,
   },
 };
